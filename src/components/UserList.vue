89615f7bdd864bcba6681e5d19de34e8b21fba66
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-18 15:16:04 +0200

    feat: add src/components/UserList.vue

---
+++ src/components/UserList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>User Profiles</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      const response = await axios.get('http://localhost:3000/api/users');
+      this.users = response.data;
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+}
+</style>
