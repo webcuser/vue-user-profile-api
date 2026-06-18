8f6ff43487c0d53a274bc72ed3d73613864ac082
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-18 15:16:46 +0200

    feat: Set up Vue.js project structure

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <template>
   <div id="app">
-    <UserList />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import UserList from './components/UserList.vue';
 export default {
   name: 'App',
-  components: {
-    UserList,
-  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Add your global styles here */
 </style>
