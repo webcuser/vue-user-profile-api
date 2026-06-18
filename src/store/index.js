0bb8cd26e8e83531c4d10dafa17116fa92ca5193
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-18 15:16:48 +0200

    feat: Set up Vue.js project structure

--- src/store/index.js
+++ src/store/index.js
@@ -4,18 +4,8 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    users: [],
-  },
-  mutations: {
-    setUsers(state, users) {
-      state.users = users;
-    },
-  },
-  actions: {
-    async fetchUsers({ commit }) {
-      const response = await axios.get('http://localhost:3000/api/users');
-      commit('setUsers', response.data);
-    },
-  },
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {},
 });
