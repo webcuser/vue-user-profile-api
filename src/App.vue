8df699e7ad56f3daf1a3dcb0b97c9e52e25f6d3e
webcuser <97110714+webcuser@users.noreply.github.com>
2026-06-18 15:16:03 +0200

    feat: add src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <UserList />
+  </div>
+</template>
+
+<script>
+import UserList from './components/UserList.vue';
+export default {
+  name: 'App',
+  components: {
+    UserList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
